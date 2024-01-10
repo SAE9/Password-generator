9aaafefd39f2984e80e4962b97e19e5b5e987a57
cat coder <s.emama@hotmail.com>
2024-01-10 21:46:27 +0000

    edited

--- script.js
+++ script.js
@@ -101,6 +101,8 @@ var upperCasedCharacters = [
 //Code should validate for each input and at least one character type should be selected
 //Once prompts are answered then the password should be generated and displayed in an alert or written to the page//
 
+
+
 function isPasswordLengthSuitable (lengthOfPassword) {
   if (lengthOfPassword < 8)  {
     return false;
@@ -126,29 +128,52 @@ function isPasswordLengthSuitable (lengthOfPassword) {
 function getPasswordOptions() {
 console.log('Get password conditions') 
 
-const lengthOfPassword = prompt('How long do you want your random password to be? (The length of the password should be minimum 8 characters long and maximun no more than 128 characters)'); //Prompts user to decide password length to fit these options
-const lengthOfPasswordAsNumber = parseInt(lengthOfPassword, 10);
+const lengthOfPassword = prompt('How long do you want your random password to be? (The length of the password should be minimum 8 characters long and maximun no more than 128 characters)') //Prompts user to decide password length to fit these options
+const lengthOfPasswordAsNumber = parseInt(lengthOfPassword, 10); //length of password before character choices
 
 if (isPasswordLengthSuitable(lengthOfPasswordAsNumber)) {
-  const isincludeLowerCase = confirm('Do you want to include lowercase characters?');
-  const isincludeUpperCase = confirm('Do you want to include Uppercase characters?');
-  const isincludeNumerical = confirm('Do you want to include Numerical characters?');
-  const isincludeSpecialCharacters = confirm('Do you want to include special characters?');
+  const lowerCase = confirm('Do you want to include lowercase characters?')
+  const upperCase = confirm('Do you want to include Uppercase characters?')
+  const numerical = confirm('Do you want to include Numerical characters?')
+  const specialChars = confirm('Do you want to include special characters?')
   //ask more questions to see if user says yes at least 1 
   //If user say yes continue loop but if not alert should appear telling user to pick at least 1
 
-  if (isincludeLowerCase || isincludeUpperCase || isincludeNumerical || isincludeSpecialCharacters ) {
+ 
+
+   if (lowerCase || upperCase || numerical || specialChars ) {
+  //   let length = lengthOfPasswordAsNumber.length
+
+  //   if (lowerCase && upperCase && specialChars)
+
+
+      let password = "";
+           // continue, to generate the password based on the user answers
+            // divide the length into however many options the user chose
+            // (e.g. when the user chose lowercase, uppercase and, special and length = 50, we divide Math.floor(50/3) = 16, 16 from lowercase, 16 from uppercase, 16 from special)
+            // check if passwordLengthAsNumber === 16 * 3, if yes continue, if no, add the rest of the character to any one of the option, uppercase = 16 + (passwordLengthAsNumber - 16 * 3)
+             //if lowerCase 
+
+            // NEW!!!
+            // Now pick 16 character from lowercase array, 16 + (passwordLengthAsNumber - 16 * 3) character from uppercasee array and 16 character from special
+            // Put it into a new array, randomise the array, then convert the to a string which will be the password!
+            // Then return the password, i.e.
+
+      // if (lowerCase) password += lowerCasedCharacters;
+      // if (upperCase) password += upperCasedCharacters;
+      // if (numerical) password += numericCharacters;
+      // if (specialChars) password += specialCharacters;
+      //suppose to link and check user answers with arrays to fit into the variable of password
+
+      // const passwordLength = password.length; // get the password length 
+      // const divideLength = Math.floor(passwordLength / password.length); // divide the users answer length with the original password length
+
+
+  // for (i=0; i < lengthOfPassword; i++) {
+  //   password[Math.floor(Math.random()*password.length)]
+  // }
+      return password;
     
-    const passwordOptions = {
-      length: lengthOfPasswordAsNumber,
-      lower: isincludeLowerCase,
-      upper: isincludeUpperCase,
-      numerical: isincludeNumerical,
-      special: isincludeSpecialCharacters,
-
-    };
-
-    return generatePassword(passwordOptions);
 
   } else {
     alert('Choose 1 character type please')
@@ -160,9 +185,6 @@ if (isPasswordLengthSuitable(lengthOfPasswordAsNumber)) {
   alert("Password length is not valid. Please make the length a minimum of 8 and maxium of 128 characters")
 }
 // function determine if the password length meets criteria and creates alert when not met
-
-
-
 console.log (lengthOfPassword);
 
 
@@ -181,20 +203,13 @@ function getRandom(arr) {
 // Function to generate password with user input
 function generatePassword() {
   console.log('Generate password')
-let createdpassword = ""; //Allows final password to show on the textarea
-getPasswordOptions(); 
+ const createdPassword=""; //Allows final password to show on the textarea
+let test = getPasswordOptions();
+console.log(test);
 
 
- return createdpassword;
+ return createdPassword;
 }
-
-
-
-
-
-
-
-
 
 
 
